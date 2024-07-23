Projects-Hikari-Paradoxes/hikari_device_xiaomi_garnet

garnet: Import Dolby Atmos blobs
Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Change-Id: Ib835d66c89afeb577dcd5d0397d8bf47f802205b

extract-files.sh
@@ -73,6 +73,12 @@ function blob_fixup() {
         vendor/etc/msm_irqbalance.conf)
             sed -i "s/IGNORED_IRQ=27,23,38$/&,115,332/" "${2}"
             ;;
+       vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service)
+            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            ;;
+       vendor/lib64/hw/audio.primary.taro.so)
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 

garnet-stuff/android_device_xiaomi_garnet

Signed-off-by: Ramadhani <ramadhanifals@gmail.com>

extract-files.sh
@@ -73,6 +73,12 @@ function blob_fixup() {
         vendor/etc/msm_irqbalance.conf)
             sed -i "s/IGNORED_IRQ=27,23,38$/&,115,332/" "${2}"
             ;;
+       vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service)
+            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            ;;
+       vendor/lib64/hw/audio.primary.parrot.so)
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 