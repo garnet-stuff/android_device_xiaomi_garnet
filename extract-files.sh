0394437b741ea961e3f684852160f573f3d12b36
Arian <arian.kulmer@web.de>
2024-09-06 14:01:17 +0200

    garnet: Use libhidlbase_shim for vendor.libdpmframework
    Change-Id: Iefe50b82b5b13bbdee10819a0c60c14240fab7ce

--- extract-files.sh
+++ extract-files.sh
@@ -77,6 +77,9 @@ function blob_fixup() {
         vendor/etc/vintf/manifest/c2_manifest_vendor.xml)
             sed -ni '/dolby/!p' "${2}"
             ;;
+        vendor/lib64/vendor.libdpmframework.so)
+            "${PATCHELF_0_17_2}" --add-needed "libhidlbase_shim.so" "${2}"
+            ;;
     esac
 }
 
